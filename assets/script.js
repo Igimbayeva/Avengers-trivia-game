8b8e1a27752f1c528b40f015552575805164dd52
markpcomer <markpcomer@gmail.com>
2024-03-04 20:45:13 -0500

    Added RequestURL and API keys

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var apiUrl = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=3a0831b3a338e3e8018a4bb3175efe3a&hash=02e6893f2e967cff388b27e4a61d3938";
+
+var publicKey = "3a0831b3a338e3e8018a4bb3175efe3a";
+var privateKey = "276c4b2db7559aa093e8754ce510f4ea31ad2da4";
+
