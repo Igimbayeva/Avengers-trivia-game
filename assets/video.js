782adbfdabac29724955a2d969e0485e11abeb0a
Igimbayeva <igimbayevadinara@gmail.com>
2024-03-11 21:34:41 -0400

    updated video.js to ensure the video is playing automatically on the page load

--- assets/video.js
+++ assets/video.js
@@ -1,44 +1,43 @@
+// Load YouTube IFrame Player API asynchronously
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '780',
-          width: '1280',
-          videoId: 'pQKYN-yR2oM',
-          playerVars: {
-            'playsinline': 1
-          },
-          events: {
+// Function to create an <iframe> (and YouTube player) after the API code downloads
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '780',
+        width: '1280',
+        videoId: 'pQKYN-yR2oM',
+        playerVars: {
+            'playsinline': 1,
+            'autoplay': 1,
+            'mute': 1  // Autoplay and mute the video
+        },
+        events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for 48.5 seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo,48500);
-          done = true;
         }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
+    });
+}
+
+// Function called when the player is ready
+function onPlayerReady(event) {
+    // No need to seek to a specific time, as autoplay will start from the beginning
+}
+
+// Function called when the player's state changes
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 40500); // Stop the video after 40.5 seconds
+        done = true;
+    }
+}
+
+// Function to stop the video
+function stopVideo() {
+    player.stopVideo();
+}
